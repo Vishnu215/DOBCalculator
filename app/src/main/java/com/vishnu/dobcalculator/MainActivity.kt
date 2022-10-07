cd37efeddd2f51eb41e0bb96c3a1915c3f457090
vishnu G <vishnug2903@gmail.com>
2022-10-07 13:20:37 +0530

    Initial commit

---
+++ app/src/main/java/com/vishnu/dobcalculator/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.vishnu.dobcalculator
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private var tvSelectedDate: TextView? = null
+    private var tvAgeInMinutes: TextView? = null
+    private var tvAge: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
+        tvAge = findViewById(R.id.tvAge)
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    private fun clickDatePicker() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(
+            this,
+            { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+                Toast.makeText(
+                    this, "Year was $selectedYear, month was ${selectedMonth + 1} and " +
+                            "day was $selectedDayOfMonth", Toast.LENGTH_LONG
+                ).show()
+
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+                tvSelectedDate?.text = selectedDate
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate = sdf.parse(selectedDate)
+                theDate?.let {
+                    val selectedDateInMinutes = theDate.time / 60000
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                    currentDate?.let {
+                        val currentDateInMinutes = currentDate.time / 60000
+                        val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+                        tvAgeInMinutes?.text = differenceInMinutes.toString()
+                        tvAge?.text = getAge(selectedYear, selectedMonth, selectedDayOfMonth)
+                    }
+                }
+            },
+            year,
+            month,
+            day
+        )
+        /*
+            There are 3,600,000 milliseconds in a hour. One hour is equal to 3.6 × 10 3 to unit of time second so
+            3600000* 24hrs = 86,400,000*/
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
+    }
+
+    private fun getAge(year: Int, month: Int, day: Int): String {
+        val dob = Calendar.getInstance()
+        val today = Calendar.getInstance()
+        dob[year, month] = day
+        var age = today[Calendar.YEAR] - dob[Calendar.YEAR]
+        if (today[Calendar.DAY_OF_YEAR] < dob[Calendar.DAY_OF_YEAR]) {
+            age--
+        }
+        return age.toString()
+    }
+}
+
+
